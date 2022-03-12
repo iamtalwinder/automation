d4317ffd9a4463f347a84999b1bec4b59ddc1e5e
Talwinder Singh <singhtalwinder790@gmail.com>
2022-03-12 16:53:26 +0530

    remove lint config

--- lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,json,md,ts,yml}': ['prettier --write', 'git add'],
-  '*.js': 'tslint -c node_modules/@pe/dev-kit/tslint.json',
-  '*.ts': 'tslint -c node_modules/@pe/dev-kit/tslint.json -p tsconfig.json'
-};
